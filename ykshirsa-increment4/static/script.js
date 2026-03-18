4f924aa2079c7048756bf15246b5fb1ffce6e343
YashKshirsagar1 <127996689+YashKshirsagar1@users.noreply.github.com>
2026-03-18 13:38:48 -0700

    Add files via upload

---
+++ ykshirsa-increment4/static/script.js
@@ -0,0 +1,131 @@
+// Numeric addition
+var x = 5;
+var y = 7;
+var z = x + y;
+console.log(z);
+
+// String concatenation
+var A = "Hello ";
+var B = "world!";
+var C = A + B;
+console.log(C);
+
+// Function definition
+function sumnPrint(x1, x2) {
+    var result = x1 + x2;
+    console.log(result);
+}
+
+// Function calls
+sumnPrint(x, y);
+sumnPrint(A, B);
+
+// Conditional statement
+if (C.length > z) {
+
+    // Nested condition check
+    if (C.length < z) {
+        console.log(z);
+    }
+    // End of nested check block
+
+} else {
+    // Code block for initial condition false
+    if (C.length < z) {
+        console.log(z);
+    } else {
+        console.log("good job!");
+    }
+}
+
+var L1 = ["Watermelon","Pineapple","Pear","Banana"];
+var L2 = ["Apple","Banana","Kiwi","Orange"];
+
+function findTheBanana(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === "Banana") {
+            alert("Banana found!");
+        }
+    }
+}
+
+//findTheBanana(L1);
+//findTheBanana(L2);
+
+function findTheBananaForEach(arr) {
+    arr.forEach(function(item) {
+        if (item === "Banana") {
+            alert("Banana found!");
+        }
+    });
+}
+
+//findTheBananaForEach(L1);
+//findTheBananaForEach(L2);
+
+// Time-based greeting
+var now = new Date();
+var hour = now.getHours();
+
+function greeting(x) {
+    var greetingElement = document.getElementById("greeting");
+
+    if (greetingElement) {
+        if (x < 5 || x >= 20) {
+            greetingElement.innerHTML = "Good night";
+        } else if (x < 12) {
+            greetingElement.innerHTML = "Good morning";
+        } else if (x < 18) {
+            greetingElement.innerHTML = "Good afternoon";
+        } else {
+            greetingElement.innerHTML = "Good evening";
+        }
+    }
+}
+
+greeting(hour);
+
+function addYear() {
+    var year = new Date().getFullYear();
+    var yearElement = document.getElementById("copyYear");
+
+    if (yearElement) {
+        yearElement.innerHTML = "© " + year + " MonoMuse Museum";
+    }
+}
+
+function ActiveNav() {
+    const navLinks = document.querySelectorAll('nav a');
+    const currentPage = window.location.pathname.split("/").pop();
+
+    navLinks.forEach(link => {
+        const linkPage = link.getAttribute("href").split("/").pop();
+
+        if (currentPage === linkPage) {
+            link.classList.add("active");
+        }
+    });
+}
+
+ActiveNav();
+
+$("#readLess").click(function(){ 
+    $("#longIntro").hide();
+    $("#readLess").hide();
+    $("#readMore").show();
+});
+
+$("#readMore").click(function(){
+    $("#longIntro").show();
+    $("#readLess").show();
+    $("#readMore").hide();
+});
+
+function showForm(date) {
+    document.getElementById("purchaseForm").style.display = "block";
+    document.getElementById("selectedDate").value = date;
+}
+
+function submitForm() {
+    alert("Redirecting to payment system.");
+}
